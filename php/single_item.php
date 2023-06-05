b2f8c3caa5f8e559d7034800dc7cd92d07378d2c
Amanuel94 <amanueltewodros94@gmail.com>
2023-06-05 23:48:45 +0300

    single-item almost done

--- php/single_item.php
+++ php/single_item.php
@@ -9,18 +9,40 @@ $data = $fetchSingleItem($recipe, $conn);
 
     <div class="top-single-item">
         <div class= "top-title">
-        <h1 class="single-item-title"><?php echo "Ethiopian Kitfo" ?> </h1><small class="single-item-author"> by <?php echo "author"; ?></small></div>
+        <h1 class="single-item-title"><?php echo "Ethiopian Kitfo" ?> </h1><a  class="single-auth-link" href="<?php echo 'user.php?id=' . $data['author'];?>"><small class="single-item-author"> by <?php
+        if(isset($data['author'])){
+        $auth = $getAuthor($data['author'], $conn);
+        $name = $auth['first_name'] . " " . $auth['last_name'];}
+        else{
+            $name = "";
+        }
+        echo  $name ?></small></a> on <i>
+        <p> <?php 
+        $dateString = $data['date_published']; // Example date string in dd-mm-yyyy format
+        // echo $dateString;
+        $date = DateTime::createFromFormat('Y-m-d', $dateString);
+
+        if ($date) {
+            $formattedDate = $date->format('M d, Y');
+            echo $formattedDate; // Output: Jun 10, 2023
+        } else {
+            echo "Invalid date format";
+        }
+        
+        ?></p></i></div>
         <div class="single-item-img">
-            <img src="<?php echo "https://www.alphafoodie.com/wp-content/uploads/2023/01/Falafel-square.jpeg"?>" alt="<?php echo $data["recipe_name"] ?>" width="600px" , height="400px">
+            <img src="<?php echo "https://www.alphafoodie.com/wp-content/uploads/2023/01/Falafel-square.jpeg" ?>" alt="<?php echo $data["recipe_name"] ?>" >
             <div class="single-item-rating">
-                <span> <?php echo $data["avg"]; ?> ( <?php echo $data["count"] ?>) reviews.</span>
+                <span id = "single-stars"></span>
+                <span class="single-count"> <?php echo round( $data["avg"], 1); ?> (<?php echo $data["count"] ?> reviews)</span>
             </div>
 
         </div>
 
 
         <div class="single-item-section">
-            <h2> Description</h1>
+           
+            <h2> Description </h1> 
                 <p><?php echo $data['description']; ?></p>
         </div>
 
@@ -62,7 +84,7 @@ $data = $fetchSingleItem($recipe, $conn);
 
         <div class = "single-item-section">
             <h2>Cuisine Type</h2>
-            <p> <?php echo $data['cuisine'];?></p>
+            <p> <?php echo ucwords($data['cuisine']);?></p>
             
         </div>
 
@@ -71,8 +93,104 @@ $data = $fetchSingleItem($recipe, $conn);
             <p> <?php echo $data['difficulty_level'];?></p>
             
         </div>
-        <div class="rate-me"> Care to rate this Recpie?</div>
+
+
+        <hr>
+<br>
+        <div id="star-rating">
+  <div class="rate-me"> Care to rate this Recpie?</div>
+  <span class="star" data-rating="1">&#9733;</span>
+  <span class="star" data-rating="2">&#9733;</span>
+  <span class="star" data-rating="3">&#9733;</span>
+  <span class="star" data-rating="4">&#9733;</span>
+  <span class="star" data-rating="5">&#9733;</span>
+</div>
+<input type="hidden" id="rating-input" name="rating">
+        
+
+
+        
 
     </div>
-</body>
+    
+    <!-- <div id="rate-stars">
+
+    <img src="../images/stars/empty.png" onclick = "rate()" onclick="rate()"/>
+    <img src="../images/stars/empty.png" onclick = "rate()" onclick="rate()"/>
+    <img src="../images/stars/empty.png" onclick = "rate()" onclick="rate()"/>
+    <img src="../images/stars/empty.png" onclick = "rate()" onclick="rate()"/>
+    <img src="../images/stars/empty.png" onclick = "rate()" onclick="rate()" />
+
+    </div> -->
+  
+
+
+    
+
+    <script >
+        let stars = document.getElementById("single-stars");
+        var i = 0;
+        let rating = "<?php echo round($data["avg"], 1);?>";
+        console.log(rating);
+        let img;
+        while (i <= 5){
+            img = document.createElement('img');
+            if( i < rating && i+1 <= rating){
+                img.setAttribute("src", "../images/stars/full.png");
+            }
+            else if(i < rating && i+1 > rating){
+                img.setAttribute("src", "../images/stars/half.png");
+            }
+            else{
+                img.setAttribute("src", "../images/stars/empty.png");
+            }
+            img.setAttribute("onclick", "rate()");
+
+            stars.appendChild(img);
+            i+=1
+
+        }
+
+
+        const stars_ = document.querySelectorAll(".star");
+        const ratingInput = document.getElementById("rating-input");
+
+        stars_.forEach((star) => {
+        star.addEventListener("mouseover", () => {
+            const rating = star.dataset.rating;
+            setRating(rating);
+        });
+
+        star.addEventListener("mouseout", () => {
+            resetRating();
+        });
+
+        star.addEventListener("click", () => {
+            const rating = star.dataset.rating;
+            ratingInput.value = rating;
+            setRating(rating);
+        });
+        });
+
+        function setRating(rating) {
+        stars_.forEach((star) => {
+            if (star.dataset.rating <= rating) {
+            star.classList.add("hover");
+            } else {
+            star.classList.remove("hover");
+            }
+        });
+        }
+
+        function resetRating() {
+        stars_.forEach((star) => {
+            star.classList.remove("hover");
+        });
+        }
+
+
+            
+
+    </script>
+
 <?php include "/opt/lampp/htdocs/mesobmagic/inc/footer.php"; ?>
