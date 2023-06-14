56ad5467b395e6e2e2ca2edfef0cab013a11f63a
Amanuel94 <amanueltewodros94@gmail.com>
2023-06-14 22:02:47 +0300

    update profile almost done

--- php/editUser.php
+++ php/editUser.php
@@ -4,7 +4,7 @@ include "/opt/lampp/htdocs/mesobmagic/inc/config/dbconn.php";
 
 $updateUser  = function($input, $conn){
 
-    $update_stmt = "UPDATE `user` SET `first_name`= ?,`last_name`=?,`age`=?,`email`=?,`country`=?,`job_title`=? ,`password`=? WHERE uid = ?";
+    $update_stmt = "UPDATE `user` SET `first_name`= ?,`last_name`=?,`dob`=?,`email`=?,`country`=?,`job_title`=? ,`password`=?, `avatar` = ? WHERE uid = ?";
 
     $stmt = $conn->prepare($update_stmt);
 
@@ -12,16 +12,19 @@ $updateUser  = function($input, $conn){
         if (!$stmt) {
             return false;
         }
+
+        
     
         $stmt->bind_param(
-            "ssissssi",
+            "ssssssssi",
             $input["first_name"],
             $input["last_name"],
-            $input['age'],
+            $input['dob'],
             $input["email"],
             $input["country"],
             $input["job_title"],
-            password_hash($input["password"], PASSWORD_DEFAULT),
+            $input["password"],
+            $input["avatar"],
             $_SESSION['uid']
 
         );
@@ -37,22 +40,170 @@ $updateUser  = function($input, $conn){
 
 };
 
-$input = [
-    "first_name"=>"john",
-    "last_name"=>"doe",
-    "age"=>20,
-    "email"=>"jdoe@gmail.com",
-    "country"=> "Other",
-    "job_title"=>"Student",
-    "password"=>"password"
-];
+$updateUserNoPW  = function($input, $conn){
 
-if($updateUser($input, $conn)){
-    echo "Successful";
+    $update_stmt = "UPDATE `user` SET `first_name`= ?,`last_name`=?,`dob`=?,`email`=?,`country`=?,`job_title`=? ,`avatar` = ? WHERE uid = ?";
+
+    $stmt = $conn->prepare($update_stmt);
+
     
+        if (!$stmt) {
+            return false;
+        }
+
+        
+    
+        $stmt->bind_param(
+            "sssssssi",
+            $input["first_name"],
+            $input["last_name"],
+            $input['dob'],
+            $input["email"],
+            $input["country"],
+            $input["job_title"],
+            // $input["password"],
+            $input["avatar"],
+            $_SESSION['uid']
+
+        );
+    
+        $result = $stmt->execute();
+    
+        if ($result) {
+            return true;
+        } else {
+          
+            return false;
+        }
+
+};
+
+
+print_r(var_dump($_POST['withPass'] == 'true'));
+if(isset($_POST['withPass']) && $_POST['withPass'] == 'true'){
+
+    $path = "avatars";
+    // ($_FILES);
+
+    $select_img = "SELECT avatar from user WHERE uid = " .$_SESSION['uid'] .";";
+
+    print_r($select_img);
+
+    $result = $conn->query($select_img);
+
+    if ($result->num_rows > 0){
+    $img = $result->fetch_assoc()['avatar'];
+    if (file_exists($img)) {
+        if (unlink($img)) {
+            echo 'File deleted successfully.';
+        } else {
+            echo 'Unable to delete the file.';
+        }
+    } else {
+        echo 'File does not exist.';
+    }
+
+    }
+
+    include "/opt/lampp/htdocs/mesobmagic/php/uploadImages.php";
+    $input = [
+        "first_name"=>$_POST['first_name'],
+        "last_name"=>$_POST['last_name'],
+        "dob"=>$_POST['dob'],
+        "email"=>$_POST['email'],
+        "country"=> $_POST['country'],
+        "job_title"=>$_POST['job'],
+        "password"=>password_hash($_POST['pass'], PASSWORD_DEFAULT),
+        "avatar"=>$imagePath
+    ];
+
+    if($updateUser($input, $conn)){
+        echo "Successful";
+        
+    }
+    else{
+        echo "AHHHHHH";
+    }
+
+
+
 }
-else{
-    echo "AHHHHHH";
+else if(isset($_POST['withPass'])){
+
+    
+
+    $path = "avatars";
+    // ($_FILES);
+
+
+    $select_img = "SELECT avatar from user WHERE uid = " .$_SESSION['uid'] .";";
+
+
+    $result = $conn->query($select_img);
+
+    if ($result->num_rows > 0){
+    $img = $result->fetch_assoc()['avatar'];
+    
+
+    }
+    print_r(!isset($_FILES['photo']));
+    if(!isset($_FILES['photo'])){
+            print_r($_FILES);
+        // Source file path on your computer
+            $sourceFilePath = $img;
+            echo $img;
+
+            // Temporary destination file path
+            $tmpFilePath = tempnam(sys_get_temp_dir(), 'tmpfile');
+
+            // Copy the file to the temporary location
+            copy($sourceFilePath, $tmpFilePath);
+
+            // Set the $_FILES array
+            $_FILES['photo'] = array(
+                'name' => basename($sourceFilePath),
+                'type' => mime_content_type($sourceFilePath),
+                'tmp_name' => $tmpFilePath,
+                'error' => 0,
+                'size' => filesize($sourceFilePath)
+            );
+
+
+    }
+    if (file_exists($img)) {
+        if (unlink($img)) {
+            echo 'File deleted successfully.';
+        } else {
+            echo 'Unable to delete the file.';
+        }
+    } else {
+        echo 'File does not exist.';
+    }
+
+    include "/opt/lampp/htdocs/mesobmagic/php/uploadImages.php";
+    $input = [
+        "first_name"=>$_POST['first_name'],
+        "last_name"=>$_POST['last_name'],
+        "dob"=>$_POST['dob'],
+        "email"=>$_POST['email'],
+        "country"=> $_POST['country'],
+        "job_title"=>$_POST['job'],
+        "password"=>password_hash($_POST['pass'], PASSWORD_DEFAULT),
+        "avatar"=>$imagePath
+    ];
+
+    if($updateUserNoPW($input, $conn)){
+        echo "Successful";
+        
+    }
+    else{
+        echo "AHHHHHH";
+    }
+
+
+
 }
+
+
 
 ?>
